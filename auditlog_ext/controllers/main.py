9c1eee8625b96c1d0d4ade8fa79ca2e96f02629f
daniel-lopez-4toangulo <daniel.lopez@4toangulo.com>
2019-11-17 23:24:26 -0600

    Deleting data param to url

--- auditlog_ext/controllers/main.py
+++ auditlog_ext/controllers/main.py
@@ -4,7 +4,7 @@ from odoo.http import request
 class ExtTrigger(http.Controller):
 
     @http.route(['/service/trigger'], type='json', auth='none')
-    def eval_trigger(self, data):
+    def eval_trigger(self):
         return {
             'rendered_html': False,
             'error': "No display found",
@@ -14,7 +14,7 @@ class ExtTrigger(http.Controller):
 class ExtAction(http.Controller):
 
     @http.route(['/service/action'], type='json', auth='none')
-    def exec_action(self, data):
+    def exec_action(self):
         return {
             'rendered_html': False,
             'error': "No display found",
