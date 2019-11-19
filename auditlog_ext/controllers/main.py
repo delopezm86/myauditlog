132ab3c989da49d9270586ea724611d11f01795f
daniel-lopez-4toangulo <daniel.lopez@4toangulo.com>
2019-11-19 16:12:53 -0600

    Uploading new models for app identification 1

--- auditlog_ext/controllers/main.py
+++ auditlog_ext/controllers/main.py
@@ -1,64 +1,27 @@
 import json
 from odoo import http
 from odoo.http import request
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class ExtTrigger(http.Controller):
 
     @http.route(['/service/trigger'], type='json', auth='none')
-    def eval_trigger(self):
+    def eval_trigger(self, **kwargs):
+        ret_dict = dict()
+        _logger.debug("*********************************")
+        _logger.debug(kwargs)
+        _logger.debug("*********************************")
         return {
             'rendered_html': False,
-            'error': "No display found",
-            'id':'1234'
+            'error': "No display found"
         }
-#
-#
-# class ExtAction(http.Controller):
-#
-#     @http.route(['/service/action'], type='json', auth='none')
-#     def exec_action(self):
-#         return {
-#             'rendered_html': False,
-#             'error': "No display found",
-#             'id': '1234'
-#         }
-#
-#
-# class ExtAuth(http.Controller):
-#
-#     @http.route(['/service/auth'], type='json', auth='none')
-#     def exec_action(self):
-#         return {
-#             'code': 200,
-#             'status': True,
-#             'id': '1234'
-#         }
-
-
-
-
-
-
-
-
-
-
-# class ExtTrigger(http.Controller):
-#
-#     @http.route(['/service/trigger'], type='http', auth='none', method='GET', cors='*')
-#     def eval_trigger(self):
-#         resp = 'This is a test'
-#         return request.make_response(['rendered_html','error'], {
-#             'Cache-Control': 'no-cache',
-#             'Content-Type': 'text/html; charset=utf-8',
-#             'Access-Control-Allow-Origin': '*',
-#             'Access-Control-Allow-Methods': 'GET',
-#         })
 
 
 class ExtAction(http.Controller):
 
-    @http.route(['/service/action'], type='http', auth='none')
+    @http.route(['/service/action'], type='json', auth='none')
     def exec_action(self):
         return {
             'rendered_html': False,
@@ -69,10 +32,13 @@ class ExtAction(http.Controller):
 
 class ExtAuth(http.Controller):
 
-    @http.route(['/service/auth'], type='http', auth='none')
+    @http.route(['/service/auth'], type='json', auth='none')
     def exec_action(self):
         return {
             'code': 200,
             'status': True,
             'id': '1234'
         }
+
+
+
--- auditlog_ext/models/nmodels.py
+++ auditlog_ext/models/nmodels.py
@@ -1,16 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import time
-import math
-import re
-
 from uuid import uuid4
-from odoo.osv import expression
-from odoo.tools.float_utils import float_round as round, float_compare
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.exceptions import UserError, ValidationError
 from odoo import api, fields, models, _, tools
-from odoo.tests.common import Form
 
 
 class SubscribeApp(models.Model):
@@ -18,4 +11,31 @@ class SubscribeApp(models.Model):
 
     name = fields.Char()
     url = fields.Char()
-    token = fields.Char('UUID', size=50, index=True, default=lambda self: str(uuid4()), copy=False)
+    current_token = fields.Char('Current UUID', compute='_get_current_token', store=True)
+    subscribe_app_line_ids = fields.One2many('subscribe.app.line', 'subscribe_app_id', 'Tokens')
+
+    @api.depends('subscribe_app_line_ids')
+    def _get_current_token(self):
+        for record in self:
+            record.current_token = record.subscribe_app_line_ids and \
+                                   record.subscribe_app_line_ids.\
+                                   sorted(key=lambda app: app.sequence, reverse=False)[0].name or ''
+
+    def create_token(self):
+        sal_obj = self.env['subscribe.app.line']
+        for record in self:
+            sequence = record.subscribe_app_line_ids and \
+                                   record.subscribe_app_line_ids.\
+                                   sorted(key=lambda app: app.sequence, reverse=False)[0].sequence or 10
+            sal_obj.create({'sequence':sequence-1})
+
+
+
+
+class SubscribeAppLine(models.Model):
+    _name = 'subscribe.app.line'
+    _order = 'sequence, id'
+
+    name = fields.Char('UUID', size=50, index=True, default=lambda self: str(uuid4()), copy=False)
+    sequence = fields.Integer(default=10, index=True)
+    subscribe_app_id = fields.Many2one('subscribe.app', 'Subscribe App')
