5d1e9bdb20e7d4ee653c576c3fb251c35692265f
daniel-lopez-4toangulo <daniel.lopez@4toangulo.com>
2019-11-18 23:42:04 -0600

    Trying GET method

--- auditlog_ext/controllers/main.py
+++ auditlog_ext/controllers/main.py
@@ -1,20 +1,63 @@
 from odoo import http
 from odoo.http import request
 
+# class ExtTrigger(http.Controller):
+#
+#     @http.route(['/service/trigger'], type='json', auth='none')
+#     def eval_trigger(self):
+#         return {
+#             'rendered_html': False,
+#             'error': "No display found",
+#             'id':'1234'
+#         }
+#
+#
+# class ExtAction(http.Controller):
+#
+#     @http.route(['/service/action'], type='json', auth='none')
+#     def exec_action(self):
+#         return {
+#             'rendered_html': False,
+#             'error': "No display found",
+#             'id': '1234'
+#         }
+#
+#
+# class ExtAuth(http.Controller):
+#
+#     @http.route(['/service/auth'], type='json', auth='none')
+#     def exec_action(self):
+#         return {
+#             'code': 200,
+#             'status': True,
+#             'id': '1234'
+#         }
+
+
+
+
+
+
+
+
+
+
 class ExtTrigger(http.Controller):
 
-    @http.route(['/service/trigger'], type='json', auth='none')
+    @http.route(['/service/trigger'], type='http', auth='none', method='GET', cors='*')
     def eval_trigger(self):
-        return {
-            'rendered_html': False,
-            'error': "No display found",
-            'id':'1234'
-        }
+        resp = 'This is a test'
+        return request.make_response(resp, {
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'text/html; charset=utf-8',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+        })
 
 
 class ExtAction(http.Controller):
 
-    @http.route(['/service/action'], type='json', auth='none')
+    @http.route(['/service/action'], type='http', auth='none')
     def exec_action(self):
         return {
             'rendered_html': False,
@@ -25,7 +68,7 @@ class ExtAction(http.Controller):
 
 class ExtAuth(http.Controller):
 
-    @http.route(['/service/auth'], type='json', auth='none')
+    @http.route(['/service/auth'], type='http', auth='none')
     def exec_action(self):
         return {
             'code': 200,
